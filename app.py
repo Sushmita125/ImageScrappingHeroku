7ab7b2513e866bcff446704b0f6598acb6b026b9
Sushmita125 <68917656+Sushmita125@users.noreply.github.com>
2020-09-09 21:20:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,13 +83,19 @@ def persist_image(folder_path:str,url:str, counter):
     except Exception as e:
         print(f"ERROR - Could not save {url} - {e}")
 
-def search_and_download(search_term: str, driver_path: str, target_path='./static', number_images=4):
+def search_and_download(search_term: str, target_path='./static', number_images=4):
     target_folder = os.path.join(target_path, '_'.join(search_term.lower().split(' '))) # make the folder name inside images with the search string
 
     if not os.path.exists(target_folder):
         os.makedirs(target_folder) # make directory using the target path if it doesn't exist already
 
-    with webdriver.Chrome(executable_path=driver_path) as wd:
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+
+    with webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options) as wd:
         res = fetch_image_urls(search_term, number_images, wd=wd, sleep_between_interactions=0.5)
 
     counter = 0
@@ -114,11 +120,14 @@ def searchImages():
         print("did not enter post")
     print('printing = ' + keyWord)
 
-    DRIVER_PATH = './chromedriver'
+
+
+
+  #  DRIVER_PATH = './chromedriver'
     search_term = keyWord
     # num of images you can pass it from here  by default it's 10 if you are not passing
     # number_images = 10
-    search_and_download(search_term=search_term, driver_path=DRIVER_PATH)
+    search_and_download(search_term=search_term)
 
     list_of_files=[]
     #files=[]
